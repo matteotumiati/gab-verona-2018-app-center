79f7e08cee5e099edc1c6dd04b12d2fc9e443efa
Matteo Tumiati <ext.matteo.tumiati@deltatre.it>
2018-04-20 18:58:22 +0200

    Retrieve of test dlls

--- Droid/appcenter-post-build.sh
+++ Droid/appcenter-post-build.sh
@@ -3,6 +3,14 @@
 echo "Logging in..."
 appcenter login --token "f1d882f81531704405a1e62ea3612e8063491608"
 
+UITestDLL=`find .. -name "GabDemo.Tests.dll" | grep bin | head -1`
+UITestBuildDir=`dirname $UITestDLL`
+APKFile=`find .. -name *.apk | head -1`
+
+echo "UITest dll: $UITestDLL"
+echo "UITest build dir: $UITestBuildDir"
+echo "APK file path: $APKFile"
+
 echo "Launching tests..."
 
 appcenter test run uitest --app "matteot/GAB-2018" \
