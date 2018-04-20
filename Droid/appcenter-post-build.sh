de008ea92c6534569ba7a2f4e42f3a7de24125d4
Matteo Tumiati <ext.matteo.tumiati@deltatre.it>
2018-04-20 18:41:00 +0200

    Changed the post build-script for login

--- Droid/appcenter-post-build.sh
+++ Droid/appcenter-post-build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Logging in..."
+appcenter login --token "f1d882f81531704405a1e62ea3612e8063491608"
+
 echo "Launching tests..."
 
 appcenter test run uitest --app "matteot/GAB-2018" \
@@ -7,6 +10,7 @@ appcenter test run uitest --app "matteot/GAB-2018" \
     --app-path "$APPCENTER_OUTPUT_DIRECTORY/matteotumiati.GabDemo-Signed.apk" \
     --test-series "ui-tests" \
     --locale "en_US" \
-    --build-dir "$APPCENTER_SOURCE_DIRECTORY/GabDemo.Tests/bin/Release/"
+    --build-dir "$APPCENTER_SOURCE_DIRECTORY/GabDemo.Tests/bin/Release/" \
+    --async
 
 echo "Current branch is $APPCENTER_BRANCH"
