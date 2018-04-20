2fd3c5b750e4948ff5c8817c09b0992f747f77fa
Matteo Tumiati <ext.matteo.tumiati@deltatre.it>
2018-04-20 18:45:52 +0200

    Changed app path for ui tests

--- Droid/appcenter-post-build.sh
+++ Droid/appcenter-post-build.sh
@@ -7,7 +7,7 @@ echo "Launching tests..."
 
 appcenter test run uitest --app "matteot/GAB-2018" \
     --devices "matteot/top-selling" \
-    --app-path "$APPCENTER_OUTPUT_DIRECTORY/matteotumiati.GabDemo-Signed.apk" \
+    --app-path "$APPCENTER_SOURCE_DIRECTORY/Droid/bin/Release/matteotumiati.GabDemo-Signed.apk" \
     --test-series "ui-tests" \
     --locale "en_US" \
     --build-dir "$APPCENTER_SOURCE_DIRECTORY/GabDemo.Tests/bin/Release/" \
