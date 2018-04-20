51f842906bfc15629b237a2052c84ddb725abb6e
Matteo Tumiati <ext.matteo.tumiati@deltatre.it>
2018-04-20 18:32:37 +0200

    Changes to post-build script

--- Droid/appcenter-post-build.sh
+++ Droid/appcenter-post-build.sh
@@ -2,12 +2,11 @@
 
 echo "Launching tests..."
 
-appcenter test run uitest \ 
-    --app "matteot/GAB-2018" \ 
+appcenter test run uitest --app "matteot/GAB-2018" \
     --devices "matteot/top-selling" \
-    --app-path "$APPCENTER_OUTPUT_DIRECTORY/matteotumiati.GabDemo-Signed.apk" \ 
-    --test-series "ui-tests" \ 
-    --locale "en_US" \ 
+    --app-path "$APPCENTER_OUTPUT_DIRECTORY/matteotumiati.GabDemo-Signed.apk" \
+    --test-series "ui-tests" \
+    --locale "en_US" \
     --build-dir "$APPCENTER_SOURCE_DIRECTORY/GabDemo.Tests/bin/Release/"
 
 echo "Current branch is $APPCENTER_BRANCH"
