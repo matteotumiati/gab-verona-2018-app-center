7b2c845ba75d1e1460ed0d2c584906574e6e89e2
Matteo Tumiati <ext.matteo.tumiati@deltatre.it>
2018-04-20 19:02:54 +0200

    Fix

--- Droid/appcenter-post-build.sh
+++ Droid/appcenter-post-build.sh
@@ -3,22 +3,14 @@
 echo "Logging in..."
 appcenter login --token "f1d882f81531704405a1e62ea3612e8063491608"
 
-UITestDLL=`find .. -name "GabDemo.Tests.dll" | grep bin | head -1`
-UITestBuildDir=`dirname $UITestDLL`
-APKFile=`find .. -name *.apk | head -1`
-
-echo "UITest dll: $UITestDLL"
-echo "UITest build dir: $UITestBuildDir"
-echo "APK file path: $APKFile"
-
 echo "Launching tests..."
 
 appcenter test run uitest --app "matteot/GAB-2018" \
     --devices "matteot/top-selling" \
-    --app-path "$APPCENTER_SOURCE_DIRECTORY/Droid/bin/Release/matteotumiati.GabDemo-Signed.apk" \
+    --app-path "../Droid/bin/Release/matteotumiati.GabDemo.apk" \
     --test-series "ui-tests" \
     --locale "en_US" \
-    --build-dir "$APPCENTER_SOURCE_DIRECTORY/GabDemo.Tests/bin/Release/" \
+    --build-dir "../GabDemo.Tests/bin/Release/" \
     --async
 
 echo "Current branch is $APPCENTER_BRANCH"
